abfb37cd1e810988a26ae3cd3748352e5c2c477e
bitwise-Aryan <aryansingh19july@gmail.com>
2025-01-03 15:02:49 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict";// treat all JS code as newer version
+
+/*alert("hello")
+alert(3+3)
+we are using nodejs, not browser it works in browser
+*/
+
+console.log(3+3)
+console.log("aryan")
+const accID=2
+//make code readable,may or maynot use semicolon
+
+
+//type of data types(see copy)
+
+console.log(typeof NULL)
+console.log(typeof accID)
